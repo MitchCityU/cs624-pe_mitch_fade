399968e8d09f613be8c3b0b18b5d3ceb967329dc
MitchCityU <72486587+MitchCityU@users.noreply.github.com>
2025-04-11 01:10:13 +0000

    commit code for pe01 through codespace

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Sam Chung</Text>
+      <Text style={styles.text}>Mitch Fade</Text>
       <Text style={styles.text}>MSCS</Text>
       <Text style={styles.text}>STC</Text>
     </View>
